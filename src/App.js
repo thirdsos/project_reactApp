5ff9bc969e561ec5658a8baeeabcb62edd957b52
thirdsos <chaiwatsos23@gmail.con>
2023-01-27 18:45:50 +0700

    002

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React ,{useEffect}from 'react'
+import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'; 
 import {FiSettings} from 'react-icons/fi'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import {Navbar  , Sidebar , ThemeSetting } from './component';
-import {Dashboard , Stacked ,Pyramid    , Line , Area , Bar , Pie    } from './pages' 
+import {Dashboard , Stacked , Line , Area , Bar , Pie    } from './pages' 
 
 import './App.css';
 
@@ -13,71 +13,8 @@ import { useStateContext } from './contexts/ContextProvider';
 
 function App() {
 
-  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+  const {  currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
 
-  useEffect(() => {
-    const currentThemeColor = localStorage.getItem('colorMode');
-    const currentThemeMode = localStorage.getItem('themeMode');
-    if (currentThemeColor && currentThemeMode) {
-      setCurrentColor(currentThemeColor);
-      setCurrentMode(currentThemeMode);
-    }
-  }, []);
-
-
-//   return (
-//     <div>
-      
-//       <BrowserRouter>
-//         <div className='flex relative dark:bg-main-dark-bg'>
-//           <div className=' fixed right-4 bottom-4' style={{zIndex:'1000'}}>
-//             <TooltipComponent content="Settings" position='"Top'>
-//               <button type='button' className=' text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{background:'blue',borderRadius:'50%'}}>
-//                 <FiSettings/>
-//               </button>
-//             </TooltipComponent>
-//           </div>
-//           {activeMenu ? (
-//             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-//               <Sidebar />
-//             </div>
-//           ) : (
-//             <div className="w-0 dark:bg-secondary-dark-bg">
-//               <Sidebar />
-//             </div>
-//           )}
-//           <div
-//             className={
-//               activeMenu
-//                 ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-//                 : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
-//             }
-//           >
-//             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-//               <Navbar />
-//             </div>
-          
-//             <div>
-//             {themeSettings && (<ThemeSetting />)}
-//               <Routes>
-//                 {/* dashboard*/}
-//                 <Route path='/' element={<Dashboard/>}/>
-//                 <Route path='/dashboard' element={<Dashboard/>}/>
-//                 {/*charts*/}
-//                 <Route path='/line' element={<Line/>}/>
-//                 <Route path='/area' element={<Area/>}/>
-//                 <Route path='/bar' element={<Bar/>}/>
-//                 <Route path='/pie' element={<Pie/>}/>
-//                 <Route path='/pyramid' element={<Pyramid/>}/>
-//                 <Route path='/stacked' element={<Stacked/>}/>
-//               </Routes>
-//             </div>
-//           </div>
-//         </div>
-//       </BrowserRouter>
-//     </div>
-//   )
-// }
 
 return (
   <div className={currentMode === 'Dark' ? 'dark' : ''}>
@@ -130,7 +67,6 @@ return (
               <Route path="/area" element={<Area />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
-              <Route path="/pyramid" element={<Pyramid />} />
               <Route path="/stacked" element={<Stacked />} />
 
             </Routes>
